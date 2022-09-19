976de53b4bb4329251c6caff5f9cd68d85917d78
Shain001 <zsyycg@gmail.com>
2022-09-19 19:19:29 +1000

    initial

---
+++ src/main/java/com/webflux/webfluxandlambda/WebfluxAndLambdaApplication.java
@@ -0,0 +1,13 @@
+package com.webflux.webfluxandlambda;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebfluxAndLambdaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebfluxAndLambdaApplication.class, args);
+    }
+
+}
